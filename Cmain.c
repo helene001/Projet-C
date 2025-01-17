634ddaffeb5629378d67368dc2b7a3bd32d8d545
NathalieHB <102826292+xWNx@users.noreply.github.com>
2025-01-17 12:13:03 +0100

    Update Cmain.c

--- Cmain.c
+++ Cmain.c
@@ -7,10 +7,8 @@ int main(int argc, char* argv[])
 {
     FILE* fichierVague = NULL;
     fichierVague = fopen("ex_vague.txt", "r");
+    
     char cagnotte[TAILLE_MAX]= "";
-
-    int ligneFichier[2] = {0};
-    char typeFichier = 0; 
 
     if (fichierVague != NULL)
     {
@@ -18,11 +16,9 @@ int main(int argc, char* argv[])
         fgets(cagnotte, TAILLE_MAX, fichierVague);
         printf("Credit restant : %s\n", cagnotte);
         
-        //gestion placement final (= affichage des vagues AVANT le debut du jeu)
-        fscanf(fichierVague, "%d %d %c", &ligneFichier[0], &ligneFichier[1], &typeFichier);
-        printf("Ligne 1 : %d %d %c\n", ligneFichier[0], ligneFichier[1], typeFichier);
-
-        afficheTerrainNeutre();
+        //afficher les vagues AVANT le debut du jeu
+        afficheVagues(fichierVague);
+        
         fclose(fichierVague);
     }
     else
