fb602d79c4f65a43b52c9b38e8e95a17821b5881
NathalieHB <102826292+xWNx@users.noreply.github.com>
2025-01-17 12:10:17 +0100

    Update Caffichage.c

--- Caffichage.c
+++ Caffichage.c
@@ -21,6 +21,40 @@ void afficheTerrainNeutre()
         }
     }
 }
-//affiche les vagues AVANT le debut du jeu
+
+//fonction qui affiche les vagues AVANT le debut du jeu
+void afficheVagues(FILE* fichierVague)
+{
+    //remplir le tableau de points 
+    char affichage[7][15]; //7 lignes et 15 colonnes
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 15; j++)
+            affichage[i][j] = '.';
+    }
+    
+    
+    int numeroTour = 0;
+    int numeroLigne = 0;
+    char typeFichier = 0; 
+    char chaine[100] = "";
+
+    //transforme le fichier en l'affichage souhaite
+    do
+    {
+    fscanf(fichierVague, "%d %d %c", &numeroTour, &numeroLigne, &typeFichier);
+    affichage[numeroLigne-1][numeroTour-1] = typeFichier;
+    } while (fgets(chaine, 100, fichierVague) != NULL);
+    
+    //afficher le tableau final
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d|", i+1);
+        for (int j = 0; j < 15; j++)
+            printf("%c\t", affichage[i][j]);
+        printf("\n");
+    }
+
+}
 
 
