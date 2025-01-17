6904317ca8385476574b671400dfb1591d83adf8
MarcoCristaudo <158455097+MarcoCristaudo@users.noreply.github.com>
2025-01-17 18:59:41 +0100

    Update struct_etud.c

--- struct_etud.c
+++ struct_etud.c
@@ -34,3 +34,32 @@ void creer_etu(int type,int ligne,int tour,Liste* l ){
         e->position=0;
    
 }
+Liste* placer(FILE * nom_fichier){
+/*FILE* fichierMechant = NULL;
+fichierMechant = fopen(nom_fichier, "r");
+if (fichierMechant == NULL) {
+    printf("Erreur lors de l'ouverture du fichier");
+    exit(1);
+}*/
+
+Liste*l=malloc(7*sizeof(Liste));
+if(l==NULL){
+    printf("erreur d'allocation");
+    exit(1); //j'arrete tout le programme
+}
+for(int i=0;i<7;i++){
+l[i].fin=NULL;
+l[i].tete=NULL;
+l[i].prochain=NULL;
+}
+
+int tour,ligne;
+char type;
+int header;
+fscanf(nom_fichier, "%d", &header);
+while (fscanf(nom_fichier, "%d %d %c", &tour, &ligne, &type) == 3) {
+    creer_etu(type,ligne,tour,l);
+    }
+//fclose(fichierMechant);
+return l;
+}
