8132515657da5bf506a7404d088aebff4f21cbc6
MarcoCristaudo <158455097+MarcoCristaudo@users.noreply.github.com>
2025-01-20 21:38:02 +0100

    Update struct_etud.c

--- struct_etud.c
+++ struct_etud.c
@@ -14,13 +14,13 @@ void creer_etu(int type,int ligne,int tour,Liste* l ){
     type_Etudiant(type,e);
     e->next=NULL;
     e->next_line=NULL;
-    if(tour==1||l[ligne-1].tete==NULL){
+    if(l[ligne-1].tete==NULL){ //si e est le premier sur sa ligne 
         e->prev_line=NULL;
         l[ligne-1].fin=e;
         l[ligne-1].tete=e;
         l[ligne-1].prochain=e;
     }
-    else{
+    else{// sinon on chaine e avec l'Etudiant qui est devant lui (qui est le dernier etudiant de l[ligne-1] avant l'ajout de e)
         e->prev_line=l[ligne-1].fin;
         l[ligne-1].fin->next_line=e;
         l[ligne-1].fin->next=e;
@@ -70,7 +70,7 @@ void type_Etudiant(int type,Etudiant* e){
     de ligne en lisant le fichier contenant les informations par rapport aux vagues. Elle utilise creer_etu(int type,int ligne,int tour,Liste* l )
     pour ajouter ces Etudiants.   */
 Liste* placer(FILE * nom_fichier){
-
+  
 Liste*l=malloc(NOMBRE_LIGNES*sizeof(Liste));
 if(l==NULL){
     printf("erreur d'allocation");
@@ -86,7 +86,10 @@ int tour,ligne;
 char type;
 int header;
 fscanf(nom_fichier, "%d", &header);
-while (fscanf(nom_fichier, "%d %d %c", &tour, &ligne, &type) == 3) {
+while (fscanf(nom_fichier, "%d %d %c", &tour, &ligne, &type) == 3) { 
+  /* tant que l'on a 3 colonnes de la forme tour d'apparition, ligne et type on 
+  lit les élèments du fichier, on les mets dans les variables tour,ligne,char et on
+  les utilise pour créer un nouvel Etudiant */
     printf("%d ",type );
     creer_etu(type,ligne,tour,l);
     }
@@ -98,7 +101,7 @@ return l;
 void liberer_liste(Liste *l){
     for (int i=0;i<NOMBRE_LIGNES;i++){
         Etudiant *courant=l[i].tete;
-        while(courant!=l[i].fin){
+        while(courant!=l[i].fin){//tant que l'on a pas le dernier Etudiant sur la ligne en passant au prochain Etudiants et en liberant la mémoire de l'étudiant courant.
             Etudiant* temp=courant;
             courant=courant->next;
             free(temp);
@@ -108,4 +111,31 @@ void liberer_liste(Liste *l){
     free(l);
     
 }
-
+/* touche_Etudiant(Etudiant *e, int degat_tourelle,int ligne ,Liste *l) permet de de prendre en compte
+    les dégats des tourelles et, dans le cas ou l'Etudiant n'a plus de Points de vie, de le tuer. */
+void touche_Etudiant(Etudiant *e, int degat_tourelle,int ligne ,Liste *l){
+    e->pointsDeVie-=degat_tourelle;
+    if (e->pointsDeVie<=0){ //si l'Etudiant meurt, on l'enlève de la liste et on libere sa mémoire
+        if (l[ligne-1].tete==e){
+            e->next->prev_line=NULL;
+            l[ligne-1].tete=e->next;
+            e->next=NULL;
+            e->next_line=NULL;
+            free(e);
+        }else if(l[ligne-1].fin==e){
+            l[ligne-1].fin=NULL;
+            l[ligne-1].tete=NULL;
+            l[ligne-1].prochain=NULL;
+            free(e);
+        }else{
+            e->prev_line->next=e->next;
+            e->prev_line->next_line=e->next_line;
+            e->next->prev_line=e->prev_line;
+            e->next_line->prev_line=e->prev_line;
+            e->next=NULL;
+            e->next_line=NULL;
+            e->prev_line=NULL;
+            free(e);
+        }
+    }
+}
