d56bd73e3b4d8d431ebd4642f1bf04a96673da27
NathalieHB <102826292+xWNx@users.noreply.github.com>
2025-01-22 22:39:33 +0100

    Update Caffichage.c

--- Caffichage.c
+++ Caffichage.c
@@ -58,3 +58,66 @@ void afficheVagues(FILE* fichierVague)
 }
 
 
+// fonction qui affiche les vagues pendant le jeu (1 par 1 avec les PV)
+void afficheVagues2(FILE* fichierVague)
+{
+    //remplir le tableau de points
+    char affichage[7][30]; // 7 lignes et 30 colonnes pour observer l'avancee des etudiants
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 30; j++)
+            affichage[i][j] = '.';
+    }
+
+    int numeroTour = 0;
+    int numeroLigne = 0;
+    char typeFichier = 0;
+    char chaine[100] = "";
+
+    // transforme le fichier en l'affichage souhaite dans un tableau de 30 colonnes
+    do
+    {
+        fscanf(fichierVague, "%d %d %c", &numeroTour, &numeroLigne, &typeFichier);
+        affichage[numeroLigne - 1][14 + numeroTour] = typeFichier; //la vague commence a la 15e colonne pour la voir apparaitre progressivement OU START A LA COLONNE 16 CA FACILITE LES CHOSES PR PV AVANCER AP
+        //printf("%c\n", affichage[numeroLigne-1][15-numeroTour]);
+      
+    } while (fgets(chaine, 100, fichierVague) != NULL);
+
+
+    // afficher le tableau final
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d| ", i + 1);
+        for (int j = 0; j < 25; j++)
+            printf("%c  ", affichage[i][j]);
+        printf("\n");
+    }
+    //c'est BON till that
+    
+
+    // afficher les tours sucessifs (observation des etudiants qui avancent)
+    //etapes : 1) avance, 2) print le tour (d'abord on avance de rien) et on le fait 9 = numeroTour fois
+        for (int tour = 1; tour <= numeroTour; tour++)
+        {
+            printf("Tour %d\n", tour);
+            //faire avancer les etudiants A FAIRE !!
+            for (int i = 0; i < 7; i++)
+            {
+                {
+                    affichage[i][14] = affichage[i][15];
+                    affichage[i][15] = affichage[i][16];
+                    affichage[i][16] = affichage[i][17];
+                    affichage[i][17] = affichage[i][18];
+                    affichage[i][18] = affichage[i][19];
+
+                }
+            }
+            /*
+            printf("%c %c ", affichage[0][14-0], affichage[0][14-0+1]);
+            affichage[0][14 - 0 - 1] = affichage[0][14 - 0];
+            printf("%c %c ", affichage[0][14-0-1], affichage[0][14-0]);
+            */
+
+        }
+}
+
