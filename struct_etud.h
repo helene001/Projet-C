f9d87442440a5019d902212d3b8e5ccd6c64c9c7
MarcoCristaudo <158455097+MarcoCristaudo@users.noreply.github.com>
2025-01-25 16:40:01 +0100

    Update struct_etud.h

--- struct_etud.h
+++ struct_etud.h
@@ -14,6 +14,7 @@ int position;
 int vitesse;
 int degats;
 int tour;
+int avancer_ou_non; //permet de controller la vitesse du type d'Etudiant 'M' (avance si avancer_ou_non=1)
 struct etudiant* next;
 struct etudiant* next_line;
 struct etudiant* prev_line;
@@ -21,15 +22,15 @@ struct etudiant* prev_line;
 
 typedef struct liste{
     Etudiant* tete;
-    Etudiant* prochain; //prochain a apparaitre
+    Etudiant* prochain; //prochain a apparaitre sur l'affichage de la partie
     Etudiant* fin;
 
 }Liste;
 
 void creer_etu(int type,int ligne,int tour,Liste * l);
 Liste* placer(FILE * nom_fichier);
-void type_Etudiant(int type,Etudiant* e);
+void type_Etudiant(int type,Etudiant* e,Liste *l);
 void liberer_liste(Liste *l);
-void afficher_liste(Liste *l);
-touche_Etudiant(Etudiant *e, int degat_tourelle,int ligne ,Liste *l);
+void touche_Etudiant(Etudiant *e, int degat_tourelle,int ligne ,Liste *l);
+void avancer(Etudiant* e, Liste *l);
 #endif
