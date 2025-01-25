1cc0de3b9a66f15fad87b03f059119e9619e02d4
MarcoCristaudo <158455097+MarcoCristaudo@users.noreply.github.com>
2025-01-25 16:37:22 +0100

    Update Caffichage.h

--- Caffichage.h
+++ Caffichage.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "struct_etud.h"
 void afficheTerrainNeutre(void);
 void afficheVagues(FILE*);
+void affichejeu(Liste *l);
+void perdu();
+void gagner();
