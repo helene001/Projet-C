3c6685ebb239ab7b4355752c5f1d4d58bbadd3f9
MarcoCristaudo <158455097+MarcoCristaudo@users.noreply.github.com>
2025-01-28 18:22:22 +0100

    Update struct_etud.h

--- struct_etud.h
+++ struct_etud.h
@@ -4,8 +4,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NOMBRE_TOURS 9  
-#define NOMBRE_LIGNES 7 
+
+#define NOMBRE_TOURS 9
+#define NOMBRE_LIGNES 7
+
+typedef struct jeu Jeu;
+
 typedef struct etudiant {
 int type;
 int pointsDeVie;
@@ -16,21 +20,17 @@ int degats;
 int tour;
 int avancer_ou_non; //permet de controller la vitesse du type d'Etudiant 'M' (avance si avancer_ou_non=1)
 struct etudiant* next;
+struct etudiant* prev;
 struct etudiant* next_line;
 struct etudiant* prev_line;
 } Etudiant;
 
-typedef struct liste{
-    Etudiant* tete;
-    Etudiant* prochain; //prochain a apparaitre sur l'affichage de la partie
-    Etudiant* fin;
-
-}Liste;
-
-void creer_etu(int type,int ligne,int tour,Liste * l);
-Liste* placer(FILE * nom_fichier);
-void type_Etudiant(int type,Etudiant* e,Liste *l);
-void liberer_liste(Liste *l);
-void touche_Etudiant(Etudiant *e, int degat_tourelle,int ligne ,Liste *l);
-void avancer(Etudiant* e, Liste *l);
+Etudiant* creer_etu(int type,int ligne,int tour,Jeu* jeu,FILE* nom_fichier);
+void placer(Jeu* jeu,FILE * nom_fichier);
+void connecte_ligne(Jeu* jeu);
+void type_Etudiant(int type,Etudiant* e,Jeu *j);
+void liberer_etudiant(Jeu* j);
+void touche_Etudiant(Etudiant *e, int degat_tourelle,int ligne,Jeu* jeu);
+void avancer(Etudiant* e);
+void afficher(Jeu *jeu);
 #endif
